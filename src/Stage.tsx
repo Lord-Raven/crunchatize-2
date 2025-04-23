e82275c12305a9510dd8e039b671a74f44931cbd
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-04-23 16:43:19 +0000

    Testing something.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -59,7 +59,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             messageState,
             chatState
         } = data;
-        console.log(this.characters);
+        console.log(characters);
         this.player = users[Object.keys(users)[0]];
         this.characters = characters;
         this.setStateFromMessageState(messageState);
